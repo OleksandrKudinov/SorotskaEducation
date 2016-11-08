dab3069ca86c4d16362adeb4a669f1ee10d8772e
Natalia.Sorotska <natalia.sorotska@gmail.com>
2016-11-08 21:57:11 +0200

    [SORED-1] Task  Solved;

--- Education.Cmd/Program.cs
+++ Education.Cmd/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Reflection.Emit;
+using System.Runtime.InteropServices;
 using Microsoft.SqlServer.Server;
 
 namespace Education.Cmd
@@ -9,16 +11,42 @@ namespace Education.Cmd
     {
         static void Main(string[] args)
         {
-            int[] i = new int[10] {1, 2, 3, 5, 7, 11, 13, 17, 19, 23};
+            int[] arr = new int[10] {1, 2, 3, 5, 7, 11, 13, 17, 19, 23};
 
             //обчислити і вивести 
             //суму
+            int sum = 0;
+            for (int j = 0; j < 10 ; j++)
+            {
+                sum += arr[j];
 
+            }
+            Console.WriteLine("sum={0}",sum);
             //добуток
+            int dob = 1;
+
+            for (int j = 0; j < 10; j++)
+            {
+                dob *= arr[j];
+
+            }
+            Console.WriteLine("dob={0}", dob);
 
             //середнє
 
+            double ser = (double)sum/10;
+            Console.WriteLine("ser={0}", ser);
             //середнє квадратичне
+            int sumkvkv = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                sumkvkv += arr[i]*arr[i];
+            }
+            double serkv = (double)sumkvkv/10;
+           double a= Math.Sqrt(serkv);
+            Console.WriteLine(a);
+
+            Console.ReadLine();
         }
     }
 }
