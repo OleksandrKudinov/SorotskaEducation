177bc74c0285647ee36c78c7f9fd0a5800089574
Oleksandr.Kudinov <oleksandr.kudinov.1993@gmail.com>
2016-11-08 21:15:05 +0300

    Added Education.cmd project;

---
+++ Education.Cmd/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection.Emit;
+using Microsoft.SqlServer.Server;
+
+namespace Education.Cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
