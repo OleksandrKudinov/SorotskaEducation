5ddd3b20eefb30a5f17e1e356900619d096fa889
Oleksandr.Kudinov <oleksandr.kudinov.1993@gmail.com>
2016-11-08 22:12:40 +0300

    [SORED-2] Added new task related search in array;

--- Education.Cmd/Program.cs
+++ Education.Cmd/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Reflection.Emit;
 using Microsoft.SqlServer.Server;
 
@@ -7,18 +8,20 @@ namespace Education.Cmd
 {
     class Program
     {
+        static int[] primeNumbers = new int[]
+        { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
+            41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
+
+        public static void Task_SORED_2()
+        {
+            //перевірка чи число є простим (шляхом порівняння)
+            //якщо число наявне в масиві - вивести повідомлення, що число просте
+        }
+
         static void Main(string[] args)
         {
-            int[] i = new int[10] {1, 2, 3, 5, 7, 11, 13, 17, 19, 23};
-
-            //обчислити і вивести 
-            //суму
-
-            //добуток
-
-            //середнє
-
-            //середнє квадратичне
+            Task_SORED_2();
+            Console.ReadLine();
         }
     }
 }
