197f85ec4e0afe7f4acfeadf12fd0ff98922526d
Natalia.Sorotska <natalia.sorotska@gmail.com>
2016-11-09 20:34:42 +0200

    [SORED-5] Task about Console.ReadLine/ReaKey complete

--- Education.Cmd/Program.cs
+++ Education.Cmd/Program.cs
@@ -14,14 +14,26 @@ namespace Education.Cmd
         { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
             41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
 
-        public static void Task_SORED_4()
+        public static void Task_SORED_5()
         {
-           
+            //Дослідити способи читання значення з клавіатури:
+            Console.ReadLine();
+            Console.ReadKey();
+            Console.WriteLine("Type anything");
+            string s = Console.ReadLine();
+            Console.WriteLine("Your tekst are {0}", s);
+            Console.ReadKey();
+            Console.WriteLine("you used readkey no paramtrs");
+            ConsoleKeyInfo sss = Console.ReadKey(true);
+            Console.WriteLine("you used readkey with paramtrs {0}", sss.Modifiers);
+
+
+
         }
 
         static void Main(string[] args)
         {
-            Task_SORED_4();
+            Task_SORED_5();
             Console.ReadLine();
         }
     }
