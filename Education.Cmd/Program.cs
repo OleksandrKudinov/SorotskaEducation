d0ea05365f2dc0d181a8975831a11a561cb2dc42
Natalia.Sorotska <natalia.sorotska@gmail.com>
2016-11-09 21:02:01 +0200

    [SORED-6] Part 1: T Parse complete

--- Education.Cmd/Program.cs
+++ Education.Cmd/Program.cs
@@ -14,18 +14,44 @@ namespace Education.Cmd
         { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
             41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
 
-        public static void Task_SORED_5()
+        public static void Task_SORED_6()
         {
-            //Дослідити способи читання значення з клавіатури:
-            Console.ReadLine();
-            Console.ReadKey();
-            Console.WriteLine("Type anything");
+            //Дослідити методи простих типів:
+            //T Parse(string value);
+            //bool TryParse(string value, out T);
+            //Використовуючи навички, здобуті в SORED-5, зчитати значення з клавіатури і перетворити у відповідні типи.
+            //Приклад програми - скріншот
+            Console.WriteLine("Write int type");
             string s = Console.ReadLine();
-            Console.WriteLine("Your tekst are {0}", s);
-            Console.ReadKey();
-            Console.WriteLine("you used readkey no paramtrs");
-            ConsoleKeyInfo sss = Console.ReadKey(true);
-            Console.WriteLine("you used readkey with paramtrs {0}", sss.Modifiers);
+            int i = int.Parse(s);
+            Console.WriteLine(i);
+            Console.WriteLine("Write double type");
+            string ss = Console.ReadLine();
+            double d = double.Parse(ss);
+            Console.WriteLine(d);
+            Console.WriteLine("Write byte type");
+            string sss = Console.ReadLine();
+            byte b = byte.Parse(sss);
+            Console.WriteLine(b);
+            Console.WriteLine("Write short type");
+            string ssss = Console.ReadLine();
+            short sh = short.Parse(ssss);
+            Console.WriteLine(sh);
+            Console.WriteLine("Write sbyte type");
+            string sssss = Console.ReadLine();
+            sbyte sb = sbyte.Parse(sssss);
+            Console.WriteLine(sb);
+            Console.WriteLine("Write bool type");
+            string ssssss = Console.ReadLine();
+            bool bol = bool.Parse(ssssss);
+            Console.WriteLine(bol);
+
+
+
+
+            Console.WriteLine();
+
+
 
 
 
@@ -33,7 +59,7 @@ namespace Education.Cmd
 
         static void Main(string[] args)
         {
-            Task_SORED_5();
+            Task_SORED_6();
             Console.ReadLine();
         }
     }
