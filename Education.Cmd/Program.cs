5fe11676cd8f1211023b686e3869adab0a4073d3
Natalia.Sorotska <natalia.sorotska@gmail.com>
2016-11-09 20:00:08 +0200

    [SORED-4] Task complete

--- Education.Cmd/Program.cs
+++ Education.Cmd/Program.cs
@@ -14,106 +14,47 @@ namespace Education.Cmd
         { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
             41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
 
-        public static void Task_SORED_2()
+        public static void Task_SORED_4()
         {
-            //перевірка чи число є простим (шляхом порівняння)
-            //якщо число наявне в масиві - вивести повідомлення, що число просте
-            int n = 42;
+            //            Використовуючи список з простих чисел від 2 до 97,
+            //            порахувати середнє квадратичне усіх остач від ділення простого 
+            //            двозначного числа на останню цифру цього числа.
 
-            bool Is = false;
+            //приклад ділення:
+            //11 / 1
+            //13 / 3
+            //17 / 7
+            //19 / 9
+            //23 / 3
+            //...
 
+            //результат програми:
+            //3.4364987719369
+            int ostacha = 0;
+            double sumKvOst = 0;
+            double divideResult = 0;
+            int coutn = 0;
             for (int i = 0; i < primeNumbers.Length; i++)
             {
-                if (n == primeNumbers[i])
+                if (primeNumbers[i] >= 10)
                 {
-                    Is = true;
-
+                    ostacha = primeNumbers[i] % 10;
+                    divideResult = ((double)primeNumbers[i]) % ostacha;
+                    sumKvOst += divideResult * divideResult;
+                    coutn++;
                 }
-            }
-
-            if (Is == true)
-            {
-                Console.WriteLine("єєє");
-            }
-            else
-            {
-                Console.WriteLine("Чоооо");
-            }
-
-
-        }
-
-        static void Task_SORED_1()
-        {
-            int[] arr = new int[10] {1, 2, 3, 5, 7, 11, 13, 17, 19, 23};
-
-            //обчислити і вивести 
-            //суму
-            int sum = 0;
-            for (int j = 0; j < 10; j++)
-            {
-                sum += arr[j];
 
             }
-            Console.WriteLine("sum={0}", sum);
-            //добуток
-            int dob = 1;
+            double serSumKvDivideResult = sumKvOst / coutn;
+            double result = Math.Sqrt(serSumKvDivideResult);
+            Console.WriteLine(result);
 
-            for (int j = 0; j < 10; j++)
-            {
-                dob *= arr[j];
-
-            }
-            Console.WriteLine("dob={0}", dob);
-
-            double ser = (double) sum/10;
-            Console.WriteLine("ser={0}", ser);
-            //середнє квадратичне
-            int sumkvkv = 0;
-            for (int i = 0; i < 10; i++)
-            {
-                sumkvkv += arr[i]*arr[i];
-            }
-            double serkv = (double) sumkvkv/10;
-            double a = Math.Sqrt(serkv);
-            Console.WriteLine(a);
-        }
-
-        public static string GetAnimalType()
-        {
-            return Console.ReadLine();
-        }
-
-        static void Task_SORED_3()
-        {
-            string animalType = GetAnimalType();
-
-            /*
-             * використати свіч кейс для виводу інформації про тваринку.
-             *дозволяється cat, dog, horse, owl, rat.
-             */
-            switch (animalType)
-            {
-                case "cat": Console.WriteLine("Cat say myau"); 
-                    break;
-                case "dog": Console.WriteLine("Dog say gag");
-                    break;
-                case "horse":Console.WriteLine("horse say frrr");
-                    break;
-                case "owl":Console.WriteLine("Owl say puhu");
-                    break;
-                case "rat":Console.WriteLine("Rat say piii");
-                    break;
-                default: Console.WriteLine("i don`t know this animal");
-                    break;
-            }
 
         }
 
         static void Main(string[] args)
         {
-            Task_SORED_2();
-            Task_SORED_3();
+            Task_SORED_4();
             Console.ReadLine();
         }
     }
