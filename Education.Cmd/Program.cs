93555ce7e1b28991b3dcb519e15de6344743def3
Natalia.Sorotska <natalia.sorotska@gmail.com>
2016-11-09 21:11:13 +0200

    [SORED-6] Part 2: TryParse complete

--- Education.Cmd/Program.cs
+++ Education.Cmd/Program.cs
@@ -21,31 +21,12 @@ namespace Education.Cmd
             //bool TryParse(string value, out T);
             //Використовуючи навички, здобуті в SORED-5, зчитати значення з клавіатури і перетворити у відповідні типи.
             //Приклад програми - скріншот
-            Console.WriteLine("Write int type");
+            Console.WriteLine("Write ");
             string s = Console.ReadLine();
-            int i = int.Parse(s);
-            Console.WriteLine(i);
-            Console.WriteLine("Write double type");
-            string ss = Console.ReadLine();
-            double d = double.Parse(ss);
-            Console.WriteLine(d);
-            Console.WriteLine("Write byte type");
-            string sss = Console.ReadLine();
-            byte b = byte.Parse(sss);
+            int b;
+            int.TryParse(s, out b);
             Console.WriteLine(b);
-            Console.WriteLine("Write short type");
-            string ssss = Console.ReadLine();
-            short sh = short.Parse(ssss);
-            Console.WriteLine(sh);
-            Console.WriteLine("Write sbyte type");
-            string sssss = Console.ReadLine();
-            sbyte sb = sbyte.Parse(sssss);
-            Console.WriteLine(sb);
-            Console.WriteLine("Write bool type");
-            string ssssss = Console.ReadLine();
-            bool bol = bool.Parse(ssssss);
-            Console.WriteLine(bol);
-
+          
 
 
 
