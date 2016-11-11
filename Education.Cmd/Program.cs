3c1c3afe9685188ec6150c5057f45dfb62aa491e
Natalia.Sorotska <natalia.sorotska@gmail.com>
2016-11-11 22:01:23 +0200

    [SORED-6] Global part complete

--- Education.Cmd/Program.cs
+++ Education.Cmd/Program.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Reflection.Emit;
-using System.Runtime.InteropServices;
-using Microsoft.SqlServer.Server;
-
 namespace Education.Cmd
 {
     class Program
@@ -14,6 +7,127 @@ namespace Education.Cmd
         { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
             41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
 
+        public static int GetIndexType()
+        {
+            int intShchos;
+            bool isParsed;
+            bool isOk = false;
+            bool isInrange = false;
+            do
+            {
+                isParsed = int.TryParse(Console.ReadLine(), out intShchos);
+                if (isParsed)
+                {
+                    isInrange = intShchos <= 3 && intShchos >= 0;
+
+                }
+                isOk = isParsed && isInrange;
+                if (!isOk)
+                {
+                    Console.WriteLine("Error, please type the correct value");
+                }
+
+
+            } while (!isOk);
+            return intShchos;
+        }
+
+        public static void ReadInt()
+        {
+            bool parse = true;
+
+            do
+            {
+                int chuslo;
+                Console.Write("Set value for type  int:");
+                string rawvalue = Console.ReadLine();
+                parse = int.TryParse(rawvalue, out chuslo);
+                if (parse == false)
+                {
+                    Console.WriteLine("Error! cannot parse '{0}' to int ", rawvalue);
+
+                }
+                else
+                {
+                    Console.WriteLine("Int value is {0}", chuslo);
+                    Console.WriteLine();
+
+                }
+            } while (!parse);
+        }
+
+        public static void ReaDouble()
+        {
+            bool parse = true;
+
+            do
+            {
+                double chuslo;
+                Console.Write("Set value for type  double:");
+                string rawvalue = Console.ReadLine();
+                parse = double.TryParse(rawvalue, out chuslo);
+                if (parse == false)
+                {
+                    Console.WriteLine("Error! cannot parse '{0}' to double ", rawvalue);
+
+                }
+                else
+                {
+                    Console.WriteLine("double value is {0}", chuslo);
+                    Console.WriteLine();
+
+                }
+            } while (!parse);
+        }
+
+        public static void ReadBool()
+        {
+            bool parse = true;
+
+            do
+            {
+                bool chuslo;
+                Console.Write("Set value for type  bool:");
+                string rawvalue = Console.ReadLine();
+                parse = bool.TryParse(rawvalue, out chuslo);
+                if (parse == false)
+                {
+                    Console.WriteLine("Error! cannot parse '{0}' to bool ", rawvalue);
+
+                }
+                else
+                {
+                    Console.WriteLine("Bool value is {0}", chuslo);
+                    Console.WriteLine();
+
+                }
+            } while (!parse);
+        }
+
+        public static void ReadByte()
+        {
+            bool parse = true;
+
+            do
+            {
+                byte chuslo;
+                Console.Write("Set value for type  byte:");
+                string rawvalue = Console.ReadLine();
+                parse = byte.TryParse(rawvalue, out chuslo);
+                if (parse == false)
+                {
+                    Console.WriteLine("Error! cannot parse '{0}' to byte ", rawvalue);
+
+                }
+                else
+                {
+                    Console.WriteLine("byte value is {0}", chuslo);
+                    Console.WriteLine();
+
+                }
+            } while (!parse);
+        }
+
         public static void Task_SORED_6()
         {
             //Дослідити методи простих типів:
@@ -21,21 +135,42 @@ namespace Education.Cmd
             //bool TryParse(string value, out T);
             //Використовуючи навички, здобуті в SORED-5, зчитати значення з клавіатури і перетворити у відповідні типи.
             //Приклад програми - скріншот
-            Console.WriteLine("Write ");
-            string s = Console.ReadLine();
-            int b;
-            int.TryParse(s, out b);
-            Console.WriteLine(b);
-          
+            string choise;
+            do
+            {
 
 
+                Console.WriteLine("bool -0\nbyte-1\nint-2\ndouble-3");
+                Console.Write("Select data type: ");
+                int indexType = GetIndexType();
+                switch (indexType)
+                {
+                    case 0:
+                        {
+                            ReadBool();
+                            break;
+                        }
+                    case 1:
+                        {
+                            ReadByte();
+                            break;
+                        }
 
-            Console.WriteLine();
-
-
-
-
-
+                    case 2:
+                        {
+                            ReadInt();
+                            break;
+                        }
+                    case 3:
+                        {
+                            ReaDouble();
+                            break;
+                        }
+                }
+                Console.Write("Would you like to continue?(y/n)");
+                choise = Console.ReadLine();
+            } while (choise == "y");
+            Console.ReadLine();
         }
 
         static void Main(string[] args)
