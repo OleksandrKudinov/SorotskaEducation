5e8a829e81ec0e3aa519ce4bfd017e2df8316a7d
Natalia.Sorotska <natalia.sorotska@gmail.com>
2016-11-11 22:56:09 +0200

    [SORED-7] Task complete

--- Education.Cmd/Program.cs
+++ Education.Cmd/Program.cs
@@ -3,179 +3,55 @@ namespace Education.Cmd
 {
     class Program
     {
-        static int[] primeNumbers = new int[]
-        { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
-            41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
-
-        public static int GetIndexType()
+        public static int ReadIntFromConsole()
         {
-            int intShchos;
-            bool isParsed;
-            bool isOk = false;
-            bool isInrange = false;
+            bool parser = true;
+            int chuslo;
             do
             {
-                isParsed = int.TryParse(Console.ReadLine(), out intShchos);
-                if (isParsed)
+                string rawIntValue = Console.ReadLine();
+                parser = int.TryParse(rawIntValue, out chuslo);
+                if (parser == false)
                 {
-                    isInrange = intShchos <= 3 && intShchos >= 0;
-
+                    Console.WriteLine("Error! cannot parse '{0}' to int ", rawIntValue);
                 }
-                isOk = isParsed && isInrange;
-                if (!isOk)
-                {
-                    Console.WriteLine("Error, please type the correct value");
-                }
-
-
-            } while (!isOk);
-            return intShchos;
+            } while (!parser);
+            return chuslo;
         }
-
-        public static void ReadInt()
+        public static void Task_SORED_7()
         {
-            bool parse = true;
 
-            do
+            Console.WriteLine("Please write value y");
+            int x = ReadIntFromConsole();
+            Console.WriteLine("Please write value x");
+            int y = ReadIntFromConsole();
+            Console.WriteLine("Write please virogidnist");
+            int v = ReadIntFromConsole();
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+            int[,] matrix = new int[x, y];
+            Random rnd = new Random();
+            for (int i = 0; i < x; i++)
             {
-                int chuslo;
-                Console.Write("Set value for type  int:");
-                string rawvalue = Console.ReadLine();
-                parse = int.TryParse(rawvalue, out chuslo);
-                if (parse == false)
+                for (int j = 0; j < y; j++)
                 {
-                    Console.WriteLine("Error! cannot parse '{0}' to int ", rawvalue);
+                    if (rnd.Next(0, 100) <= v)
+                    {
+                        matrix[i, j] = 1;
+                    }
+                    else
+                    {
+                        matrix[i, j] = 0;
+                    }
+                    Console.Write(matrix[i, j]);
 
                 }
-                else
-                {
-                    Console.WriteLine("Int value is {0}", chuslo);
-                    Console.WriteLine();
-
-                }
-            } while (!parse);
+                Console.WriteLine();
+            }
         }
-
-        public static void ReaDouble()
-        {
-            bool parse = true;
-
-            do
-            {
-                double chuslo;
-                Console.Write("Set value for type  double:");
-                string rawvalue = Console.ReadLine();
-                parse = double.TryParse(rawvalue, out chuslo);
-                if (parse == false)
-                {
-                    Console.WriteLine("Error! cannot parse '{0}' to double ", rawvalue);
-
-                }
-                else
-                {
-                    Console.WriteLine("double value is {0}", chuslo);
-                    Console.WriteLine();
-
-                }
-            } while (!parse);
-        }
-
-        public static void ReadBool()
-        {
-            bool parse = true;
-
-            do
-            {
-                bool chuslo;
-                Console.Write("Set value for type  bool:");
-                string rawvalue = Console.ReadLine();
-                parse = bool.TryParse(rawvalue, out chuslo);
-                if (parse == false)
-                {
-                    Console.WriteLine("Error! cannot parse '{0}' to bool ", rawvalue);
-
-                }
-                else
-                {
-                    Console.WriteLine("Bool value is {0}", chuslo);
-                    Console.WriteLine();
-
-                }
-            } while (!parse);
-        }
-
-        public static void ReadByte()
-        {
-            bool parse = true;
-
-            do
-            {
-                byte chuslo;
-                Console.Write("Set value for type  byte:");
-                string rawvalue = Console.ReadLine();
-                parse = byte.TryParse(rawvalue, out chuslo);
-                if (parse == false)
-                {
-                    Console.WriteLine("Error! cannot parse '{0}' to byte ", rawvalue);
-
-                }
-                else
-                {
-                    Console.WriteLine("byte value is {0}", chuslo);
-                    Console.WriteLine();
-
-                }
-            } while (!parse);
-        }
-
-        public static void Task_SORED_6()
-        {
-            //Дослідити методи простих типів:
-            //T Parse(string value);
-            //bool TryParse(string value, out T);
-            //Використовуючи навички, здобуті в SORED-5, зчитати значення з клавіатури і перетворити у відповідні типи.
-            //Приклад програми - скріншот
-            string choise;
-            do
-            {
-
-
-                Console.WriteLine("bool -0\nbyte-1\nint-2\ndouble-3");
-                Console.Write("Select data type: ");
-                int indexType = GetIndexType();
-                switch (indexType)
-                {
-                    case 0:
-                        {
-                            ReadBool();
-                            break;
-                        }
-                    case 1:
-                        {
-                            ReadByte();
-                            break;
-                        }
-
-                    case 2:
-                        {
-                            ReadInt();
-                            break;
-                        }
-                    case 3:
-                        {
-                            ReaDouble();
-                            break;
-                        }
-                }
-                Console.Write("Would you like to continue?(y/n)");
-                choise = Console.ReadLine();
-            } while (choise == "y");
-            Console.ReadLine();
-        }
-
         static void Main(string[] args)
         {
-            Task_SORED_6();
+            Task_SORED_7();
             Console.ReadLine();
         }
     }
