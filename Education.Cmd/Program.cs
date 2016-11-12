83a004ef28eb4e40b448d97cabf20549d9fc0cad
Natalia.Sorotska <natalia.sorotska@gmail.com>
2016-11-12 18:04:28 +0200

    [SORED-11] Task complete

--- Education.Cmd/Program.cs
+++ Education.Cmd/Program.cs
@@ -1,110 +1,62 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
 namespace Education.Cmd
 {
     class Program
     {
-        public static int ReadIntFromConsole()
+
+        public static void Counter_Task_SORED_11()
         {
-            bool parser = true;
-            int chuslo;
-            do
+            Console.Write("Please write x ");
+            int x;
+            string xs = Console.ReadLine();
+            bool resultX = int.TryParse(xs, out x);
+            Console.Write("Please write y ");
+            int y;
+            string ys = Console.ReadLine();
+            bool resultY = int.TryParse(ys, out y);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine("Please tap button up down left reight for move smile");
+            while (true)
             {
-                string rawIntValue = Console.ReadLine();
-                parser = int.TryParse(rawIntValue, out chuslo);
-                if (parser == false)
+                ConsoleKeyInfo info = Console.ReadKey(false);
+                var key = info.Key;
+                switch (info.Key)
                 {
-                    Console.WriteLine("Error! cannot parse '{0}' to int ", rawIntValue);
-                }
-            } while (!parser);
-            return chuslo;
-        }
-        public static void ShowMatrix(int[,] matrix, int x, int y)
-        {
-            for (int i = 0; i < x + 2; i++)
-            {
-                Console.Write("_");
-
-            }
-            Console.WriteLine();
-            for (int j = 0; j < y; j++)
-            {
-
-                Console.Write("|");
-
-                for (int i = 0; i < x; i++)
-                {
-
-                    switch (matrix[i, j])
-                    {
-                        case 0:
-                            Console.Write("+");
-                            break;
-                        case 1:
-                            Console.Write((char)0x2665);
-                            break;
-                        case 2:
-                            Console.Write("☺");
-                            break;
-                    }
-
-                }
-                Console.WriteLine("|");
-            }
-            for (int i = 0; i < x + 2; i++)
-            {
-                Console.Write("-");
-
-            }
-            Console.WriteLine();
-        }
-        public static void Task_SORED_10()
-        {
-            Console.WriteLine("Please write value x");
-            int x = ReadIntFromConsole();
-            Console.WriteLine("Please write value y");
-            int y = ReadIntFromConsole();
-            Console.WriteLine("Write please virogidnist");
-            int v = ReadIntFromConsole();
-            Console.Write("Please write coordinate x for persone ");
-            int personX = ReadIntFromConsole();
-            Console.Write("Please write coordinate y for persone ");
-            int personY = ReadIntFromConsole();
-
-            if (personX < x && personX >= 0 && personY < y && personY >= 0)
-            {
-                int[,] matrix = new int[x, y];
-                Random rnd = new Random();
-                for (int j = 0; j < y; j++)
-                {
-                    for (int i = 0; i < x; i++)
-                    {
-                        if (rnd.Next(0, 100) <= v)
+                    case ConsoleKey.UpArrow:
                         {
-                            matrix[i, j] = 1;
+                            y++;
+
                         }
-                        else
+                        break;
+                    case ConsoleKey.DownArrow:
                         {
-                            matrix[i, j] = 0;
+                            y--;
                         }
-                    }
-                }
-                matrix[personX, personY] = 2;
-                ShowMatrix(matrix, x, y);
-            }
-            else
-            {
-                Console.WriteLine("Write corect  x,y for persone");
-            }
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        {
+                            x--;
 
+                        } break;
 
+                    case ConsoleKey.RightArrow:
+                        {
+                            x++;
+                        }
+                        break;
+                     }
+                //Console.WriteLine("x= {0}",x);
+                //Console.WriteLine("y= {0}", y);
+            }
         }
         static void Main(string[] args)
         {
-            Console.WriteLine((char)0x2665);
-            string s = ((char)0x2665).ToString();
-            Console.WriteLine(s);
-            Task_SORED_10();
+            Counter_Task_SORED_11();
             Console.ReadLine();
         }
-    }
+        }
 }
